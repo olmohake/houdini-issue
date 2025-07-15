b8bc48c944915b901c560dba150825b0a360844b
Yannik Olmo Hake <yannik.hake@gmail.com>
2025-07-15 20:17:44 +0200

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Reproduction for Houdini Issue <a target="_blank" href="https://github.com/HoudiniGraphql/houdini/issues/1514">#1514</a></h1>
 <p><b>public hasura ddn endpoint:</b> https://useful-pony-7128-053034adce.ddn.hasura.app/graphql</p>
 <p><b>github repo containing this example:</b> <a  href="https://github.com/olmohake/houdini-issue" target="_blank">olmohake/houdini-issue</a></p>
+<p>Query working as expected: <a  href="/items">Items</a></p>
 <p>Query including Aggregations not working as expect: <a  href="/items_aggregate">Aggregated Items</a></p>
